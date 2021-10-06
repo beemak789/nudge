eb4055075c4a16dde108e8086deac3e54d877e1a
Miranda Stopa <miranda.stopa@gmail.com>
2021-10-06 18:33:52 -0400

    logout works

--- Components/Main.js
+++ Components/Main.js
@@ -9,8 +9,8 @@ import {
   Screens1Navigator,
   Screens2Navigator,
   Screens3Navigator,
-  Screens4Navigator,
 } from "../services/Stacks";
+import LogOut from "./LogOut";
 import { Text, View } from "react-native";
 import { firebase } from "../config/firebase";
 
@@ -18,15 +18,7 @@ const Tab = createBottomTabNavigator();
 
 const Main = () => {
   const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState(false);
-
-  if (loading) {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Loading...</Text>
-      </View>
-    );
-  }
+  const [user, setUser] = useState({});
 
   useEffect(() => {
     const usersRef = firebase.firestore().collection("users");
@@ -49,9 +41,28 @@ const Main = () => {
     });
   }, []);
 
+  if (loading) {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  const logOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    setUser({});
+  };
+
   return (
     <Tab.Navigator
-      initialRouteName="Screen 1"
+      initialRouteName="Screens 1"
       screenOptions={{
         activeTintColor: "#6ede8a",
         itemStyle: { marginVertical: 10 },
@@ -61,108 +72,33 @@ const Main = () => {
         },
       }}
     >
-      {!user
-        ? loggedOutDrawer.map((tab) => (
-            <Tab.Screen
-              key={tab.name}
-              name={tab.name}
-              options={{
-                // below is for rendering the icons
-
-                // tabBarIcon: ({color, focused}) => {
-                //   let iconName;
-
-                //   if (tab.name === 'Log In') {
-                //     return (
-                //       <Feather
-                //         name="log-in"
-                //         focused={focused}
-                //         size={25}
-                //         color={color}
-                //       />
-                //     );
-                //   } else if (tab.name === 'Sign Up') {
-                //     return (
-                //       <AntDesign
-                //         name="adduser"
-                //         focused={focused}
-                //         size={25}
-                //         color={color}
-                //       />
-                //     );
-                //   }
-                // },
-
-                tabBarActiveTintColor: "#6ede8a",
-                tabBarInactiveTintColor: "#97a97c",
-              }}
-              component={
-                tab.name === "Log In"
-                  ? (props) => <LogIn {...props} />
-                  : (props) => <SignUp {...props} />
-              }
-            />
-          ))
-        : loggedInDrawer.map((tab) => (
-            <Tab.Screen
-              key={tab.name}
-              name={tab.name}
-              options={{
-                // below is for rendering the icons
-                //   tabBarIcon: ({color, focused}) => {
-                //     let iconName;
-
-                //     if (tab.name === 'Screen 1') {
-                //       return (
-                //         <Ionicons
-                //           name="person-circle-outline"
-                //           focused={focused}
-                //           size={25}
-                //           color={color}
-                //         />
-                //       );
-                //     } else if (tab.name === 'Screen 2') {
-                //       return (
-                //         <Feather
-                //           name="calendar"
-                //           focused={focused}
-                //           size={25}
-                //           color={color}
-                //         />
-                //       );
-                //     } else if (tab.name === 'Screen 3') {
-                //       <Feather
-                //         name="log-out"
-                //         focused={focused}
-                //         size={25}
-                //         color={color}
-                //       />;
-                //     } else {
-                //       return (
-                //         <Ionicons
-                //           name="people-outline"
-                //           focused={focused}
-                //           size={25}
-                //           color={color}
-                //         />
-                //       );
-                //     }
-                //   },
-
-                tabBarActiveTintColor: "#6ede8a",
-                tabBarInactiveTintColor: "#97a97c",
-              }}
-              component={
-                tab.name === "Screens 1"
-                  ? (props) => <Screens1Navigator {...props} />
-                  : tab.name === "Screens 2"
-                  ? (props) => <Screens2Navigator {...props} />
-                  : tab.name === "Screens 3"
-                  ? (props) => <Screens3Navigator {...props} />
-                  : (props) => <Screens4Navigator {...props} />
-              }
-            />
-          ))}
+      {!user.id ? (
+        <>
+          <Tab.Screen
+            name="Log In"
+            component={(props) => <LogIn {...props} />}
+          />
+          <Tab.Screen
+            name="Sign Up"
+            component={(props) => <SignUp {...props} />}
+          />
+        </>
+      ) : (
+        <>
+          <Tab.Screen name="Screens 1">
+            {(props) => <Screens1Navigator {...props} />}
+          </Tab.Screen>
+          <Tab.Screen name="Screens 2">
+            {(props) => <Screens2Navigator {...props} />}
+          </Tab.Screen>
+          <Tab.Screen name="Screens 3">
+            {(props) => <Screens3Navigator {...props} />}
+          </Tab.Screen>
+          <Tab.Screen name="Log Out">
+            {(props) => <LogOut {...props} logOut={logOut} />}
+          </Tab.Screen>
+        </>
+      )}
     </Tab.Navigator>
   );
 };
--- Components/Screen1.js
+++ Components/Screen1.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, SafeAreaView } from "react-native";
+import { StyleSheet, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import { firebase } from "../config/firebase";
 
 const Screen1 = (props) => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>This is screen 1</Text>
+      <Text>This is Screen 1</Text>
     </SafeAreaView>
   );
 };
