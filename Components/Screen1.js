ec0b141f4361986368a70d94b48417938279a0ec
Nicole <nicole.pan1127@gmail.com>
2021-10-07 14:00:55 -0400

    fixed logout bug and some inline errors working on push

--- Components/Screen1.js
+++ Components/Screen1.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, SafeAreaView, Button } from "react-native";
 import { firebase } from "../config/firebase";
 
 const Screen1 = (props) => {
   return (
     <SafeAreaView style={styles.container}>
       <Text>This is Screen 1</Text>
+      <Button
+        title="Press to Send Notification"
+        onPress={async () => {
+          console.log('button presed', props)
+          // await props.sendPushNotification(props.expoPushToken);
+        }}
+      />
     </SafeAreaView>
   );
 };
