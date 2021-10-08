9137b93a8668ecfc7832dc172c50adeddb97cafb
Brandy Mak <bmak789@gmail.com>
2021-10-08 15:06:30 -0400

    user redux store

--- store/user.js
+++ store/user.js
@@ -1,9 +1,95 @@
+import { firebase } from '../config/firebase';
 const SET_USER = 'SET_USER';
 
-export const setUser = () => {
+
+export const setUser = (user) => {
   return {
     type: SET_USER,
     user,
+  };
+};
+
+export const logInUser = (email, password) => {
+  return async (dispatch) => {
+    try {
+      await firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(async (response) => {
+          const uid = response.user.uid;
+          const usersRef = firebase.firestore().collection('users');
+          usersRef
+            .doc(uid)
+            .get()
+            .then((firestoreDocument) => {
+              if (!firestoreDocument.exists) {
+                alert('User does not exist anymore.');
+                return;
+              }
+              const data = firestoreDocument.data();
+              dispatch(setUser(data));
+            })
+            .catch((error) => {
+              alert(error);
+            });
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const logOutUser = () => {
+  return async (dispatch) => {
+    try {
+      await firebase
+        .auth()
+        .signOut()
+        .catch(function (error) {
+          console.log(error);
+        });
+
+      dispatch(setUser({}));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const signUpUser = (email, password, first, last) => {
+  return async (dispatch) => {
+    try {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(async (response) => {
+          const uid = response.user.uid;
+          await AsyncStorage.setItem('USER_ID', uid);
+          const data = {
+            id: uid,
+            email,
+            fullName: first + last,
+          };
+
+          const usersRef = firebase.firestore().collection('users');
+
+          usersRef
+            .doc(uid)
+            .set(data)
+            .catch((error) => {
+              alert(error);
+            });
+        })
+        .catch((error) => {
+          alert(error);
+        });
+      dispatch(setUser(data));
+    } catch (err) {
+      console.log(err);
+    }
   };
 };
 
