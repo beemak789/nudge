d936ecafcbda0202f8dd8f8396c55ee234b9800e
Miranda Stopa <miranda.stopa@gmail.com>
2021-10-12 14:27:47 -0400

    added and configured redux-persist

--- App.js
+++ App.js
@@ -3,13 +3,16 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import Main from './Components/Main';
-import store from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Provider store = {store}>
-      <Main />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Main />
+        </PersistGate>
       </Provider>
     </NavigationContainer>
   );
--- store/index.js
+++ store/index.js
@@ -1,16 +1,25 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
 import task from './task';
 import user from './user';
 import location from './location';
 import places from './places';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const appReducer = combineReducers({ task, user, location, places });
 
-const store = createStore(
-  appReducer,
-  applyMiddleware(thunkMiddleware, createLogger())
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, appReducer);
+
+export const store = createStore(
+  persistedReducer,
+  applyMiddleware(thunkMiddleware)
 );
 
-export default store;
+export const persistor = persistStore(store);
